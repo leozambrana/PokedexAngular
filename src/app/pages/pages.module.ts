e66bfa41abb5ecdab580a3616c0cc84ddf031a0c
leonardo.zambrana <leonardozambrana1@unesc.net>
2022-11-14 18:59:56 -0300

    second-commit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//route
 import { RoutingModule } from './routing.module';
+
+//module
+import { SharedModule } from './../shared/shared.module';
 
 //Pages
 import { HomeComponent } from './home/home.component';
@@ -16,7 +21,9 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule,
+
   ]
 })
 export class PagesModule { }
---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,24 @@
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+public getAllPokemons: any;
+
+  constructor(private pokeapiservice: PokeApiService) { }
+
+  ngOnInit(): void {
+    this.pokeapiservice.apiListAllPokemons.subscribe(
+      res=> {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons);
+      }
+    );
+  }
+
+}
